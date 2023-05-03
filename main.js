bc622a123c53067f29eeac38c5013c58eb10c8b3
carvso <123869730+carvso@users.noreply.github.com>
2023-05-03 08:16:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css';
 
 import * as THREE from '/three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls';
+import {GLTFLoader} from '/three/examples/jsm/loaders/GLTFLoader';
 
 
 var redpi, screw, tomb;
